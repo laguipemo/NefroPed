18ebf188496ed53fe55baab0eaceb5fec3a8b757
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2026-03-02 00:50:32 +0100

    feat(project): crear y sincronizar correctamente modulos domain y common

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,16 +26,6 @@ rootProject.name = "NefroPed"
 include(":app")
 
 // Nuevos módulos
-include(":core:domain")
-include(":core:data")
-include(":core:local")
-include(":core:common")
-include(":features:auth")
-include(":features:chat")
-include(":features:courses")
-include(":features:onboarding")
-include(":features:splash")
-include(":designsystem")
-include(":di")
 include(":domain")
-include(":domain")
+include(":common")
+
