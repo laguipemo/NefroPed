eac2e6306e678b2ea6fcfb2f2964e9eb91947338
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2026-03-17 16:36:35 +0100

    chore(project): actualiza changelog para release versión 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
